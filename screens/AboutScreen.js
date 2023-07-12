707bb5de0c76bac69f3475060a830210489d7261
Casey Hatcher <casey.willetts@gmail.com>
2023-07-12 14:07:35 -0400

    Activity Indicator

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -1,30 +1,55 @@
- import { Text, ScrollView } from 'react-native';
+import { Text, ScrollView } from 'react-native';
 import { ListItem } from 'react-native-elements';
 import { Avatar, Card } from 'react-native-elements';
 import { useSelector } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl'
+import Loading from '../components/LoadingComponent';
 
+function Mission() {
+    return (
+        <Card>
+            <Card.Title>Our Mission</Card.Title>
+            <Card.Divider />
+            <Text style={{ margin: 10 }}>
+                We present a curated database of the best campsites in the vast woods
+                and backcountry of the World Wide Web Wilderness. We increase access
+                to adventure for the public while promoting safe and respectful use of
+                resources. The expert wilderness trekkers on our staff personally
+                verify each campsite to make sure that they are up to our standards.
+                We also present a platform for campers to share reviews on campsites
+                they have visited with each other.
+            </Text>
+        </Card>
+    );
+};
 
-    function Mission() {
+const AboutScreen = () => {
+    const partners = useSelector((state) => state.partners);
+
+    if (partners.isLoading) {
         return (
+            <ScrollView>
+                <Mission />
+                <Card>
+                    <Card.Title>Community Partners</Card.Title>
+                    <Card.Divider />
+                    <Loading />
+                </Card>
+            </ScrollView>
+        )
+    }
+    if (partners.errMess) {
+        return (
+            <ScrollView>
+            <Mission />
             <Card>
-                <Card.Title>Our Mission</Card.Title>
+                <Card.Title>Community Partners</Card.Title>
                 <Card.Divider />
-                <Text style={{ margin: 10 }}>
-                    We present a curated database of the best campsites in the vast woods
-                    and backcountry of the World Wide Web Wilderness. We increase access
-                    to adventure for the public while promoting safe and respectful use of
-                    resources. The expert wilderness trekkers on our staff personally
-                    verify each campsite to make sure that they are up to our standards.
-                    We also present a platform for campers to share reviews on campsites
-                    they have visited with each other.
-                </Text>
+                <Text>{partners.errMess}</Text>
             </Card>
-        );
-    };
-
-    const AboutScreen = () => {
-        const partners = useSelector((state) => state.partners);
+        </ScrollView>
+        )
+    }
 
     return (
         <ScrollView>
@@ -34,7 +59,7 @@ import { baseUrl } from '../shared/baseUrl'
                 <Card.Divider />
                 {partners.partnersArray.map((partner) => (
                     <ListItem key={partner.id}>
-                        <Avatar rounded source={{ uri: baseUrl + partner.image}} />
+                        <Avatar rounded source={{ uri: baseUrl + partner.image }} />
                         <ListItem.Content>
                             <ListItem.Title>{partner.name}</ListItem.Title>
                             <ListItem.Subtitle>{partner.description}</ListItem.Subtitle>
